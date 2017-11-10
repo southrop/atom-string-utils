14d9e6d7e3feb92858d1bc6e41708e0c71fbb5e0
Southrop <southrop113@gmail.com>
2017-11-10 17:31:51 +0900

    Add support for URL encoding

--- spec/string-utils-spec.js
+++ spec/string-utils-spec.js
@@ -18,7 +18,7 @@ describe('String Utils', () => {
     runs(() => {
       return callback();
     });
-  }
+  };
 
   beforeEach(() => {
     workspace = atom.views.getView(atom.workspace);
@@ -60,7 +60,7 @@ describe('String Utils', () => {
       return activate('string-utils:reverse-selection', () => {
         expect(editor.getText()).toBe('This is a test string&$^%#!\na 5121sdf');
       });
-    })
+    });
   });
 
   describe('Space↔Tab Conversion', () => {
@@ -86,21 +86,35 @@ describe('String Utils', () => {
       return activate('string-utils:convert-spaces-to-tabs', () => {
         return activate('string-utils:convert-tabs-to-spaces', () => {
           expect(editor.getText()).toBe('if (test) {\n  for (condition) {\n    return that;\n  }\n  return  this;\n}');
-        })
+        });
       });
     });
   });
 
   describe('Base64 Conversion', () => {
-    it('converts text into base64 successfully', () => {
+    it('does nothing to plain text if there is no selection', () => {
+      editor.setText('This is a test string!#%^%$&\\n 1215あ啊');
+      return activate('string-utils:encode-in-base64', () => {
+        expect(editor.getText()).toBe('This is a test string!#%^%$&\\n 1215あ啊');
+      });
+    });
+
+    it('converts plain text into base64 successfully', () => {
       editor.setText('This is a test string!#%^$&\\n 1215あ啊');
       editor.selectAll();
-      return activate('string-utils:encode-to-base64', () => {
+      return activate('string-utils:encode-in-base64', () => {
         expect(editor.getText()).toBe('VGhpcyBpcyBhIHRlc3Qgc3RyaW5nISMlXiQmXG4gMTIxNeOBguWVig==');
       });
     });
 
-    it('converts base64 into text successfully', () => {
+    it('does nothing to base64 if there is no selection', () => {
+      editor.setText('VGhpcyBpcyBhIHRlc3Qgc3RyaW5nISMlXiQmXG4gMTIxNeOBguWVig==');
+      return activate('string-utils:decode-from-base64', () => {
+        expect(editor.getText()).toBe('VGhpcyBpcyBhIHRlc3Qgc3RyaW5nISMlXiQmXG4gMTIxNeOBguWVig==');
+      });
+    });
+
+    it('converts base64 into plain text successfully', () => {
       editor.setText('VGhpcyBpcyBhIHRlc3Qgc3RyaW5nISMlXiQmXG4gMTIxNeOBguWVig==');
       editor.selectAll();
       return activate('string-utils:decode-from-base64', () => {
@@ -111,7 +125,7 @@ describe('String Utils', () => {
     it('can undo itself', () => {
       editor.setText('This is a test string!#%^$&\\n 1215あ啊');
       editor.selectAll();
-      return activate('string-utils:encode-to-base64', () => {
+      return activate('string-utils:encode-in-base64', () => {
         return activate('string-utils:decode-from-base64', () => {
           expect(editor.getText()).toBe('This is a test string!#%^$&\\n 1215あ啊');
         });
@@ -119,4 +133,45 @@ describe('String Utils', () => {
     });
   });
 
+  describe('URL Notation Conversion', () => {
+    it('does nothing to plain text if there is no selection', () => {
+      editor.setText('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+      return activate('string-utils:encode-in-url-encoding', () => {
+        expect(editor.getText()).toBe('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+      });
+    });
+
+    it('converts plain text into url notation', () => {
+      editor.setText('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+      editor.selectAll();
+      return activate('string-utils:encode-in-url-encoding', () => {
+        expect(editor.getText()).toBe('http%3A%2F%2Flocalhost%3A30001%2Fdata%3Fzip%3D47401%26utc_begin%3D2013-8-1%2000%3A00%3A00%26utc_end%3D2013-8-2%2000%3A00%3A00%26country_code%3DUSA');
+      });
+    });
+
+    it('does nothing to url notation text if there is no selection', () => {
+      editor.setText('http%3A%2F%2Flocalhost%3A30001%2Fdata%3Fzip%3D47401%26utc_begin%3D2013-8-1%2000%3A00%3A00%26utc_end%3D2013-8-2%2000%3A00%3A00%26country_code%3DUSA');
+      return activate('string-utils:decode-from-url-encoding', () => {
+        expect(editor.getText()).toBe('http%3A%2F%2Flocalhost%3A30001%2Fdata%3Fzip%3D47401%26utc_begin%3D2013-8-1%2000%3A00%3A00%26utc_end%3D2013-8-2%2000%3A00%3A00%26country_code%3DUSA');
+      });
+    });
+
+    it('converts url notation into plain text', () => {
+      editor.setText('http%3A%2F%2Flocalhost%3A30001%2Fdata%3Fzip%3D47401%26utc_begin%3D2013-8-1%2000%3A00%3A00%26utc_end%3D2013-8-2%2000%3A00%3A00%26country_code%3DUSA');
+      editor.selectAll();
+      return activate('string-utils:decode-from-url-encoding', () => {
+        expect(editor.getText()).toBe('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+      });
+    });
+
+    it('can undo itself', () => {
+      editor.setText('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+      editor.selectAll();
+      return activate('string-utils:encode-in-url-encoding', () => {
+        return activate('string-utils:decode-from-url-encoding', () => {
+          expect(editor.getText()).toBe('http://localhost:30001/data?zip=47401&utc_begin=2013-8-1 00:00:00&utc_end=2013-8-2 00:00:00&country_code=USA');
+        });
+      });
+    });
+  });
 });
